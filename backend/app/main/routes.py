4a2d67847527328a3ea7dc6785d3cff220ab4642
Норманев Дмитрий <dimanormanev@yandex.ru>
2023-02-14 16:23:10 +0300

    Заготовка

--- backend/app/main/routes.py
+++ backend/app/main/routes.py
@@ -1,19 +1,31 @@
 from app import socketio
-from flask_socketio import send, emit
+from flask_socketio import send, emit, Namespace
 from app.main import bp
 
 
-@socketio.on('my_response')
-def handle_message(message):
-    print(message)
-    emit('test_data', message)
-
-
-@socketio.on('connect')
-def test_connect():
-    print(5555)
+# @socketio.on('my_response')
+# def handle_message(message):
+#     print(message)
+#     emit('test_data', message)
+#
+#
+# @socketio.on('connect')
+# def test_connect():
+#     print(5555)
 
 
 @bp.route('/123')
 def test():
     return '123'
+
+
+class MyCustomNamespace(Namespace):
+    def on_connect(self):
+        print('connect')
+
+    def on_my_response(self, message):
+        print(message)
+        emit('test_data', message)
+
+
+socketio.on_namespace(MyCustomNamespace('/test'))
