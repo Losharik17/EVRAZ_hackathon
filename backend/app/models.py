2fde79aedcac03e172ac87d57f254d3ca46a3ecd
Норманев Дмитрий <dimanormanev@yandex.ru>
2023-02-16 17:49:02 +0300

    Заготовка

--- backend/app/models.py
+++ backend/app/models.py
@@ -20,18 +20,22 @@ class Eksgauster(db.Model):
 
     id: int = db.Column(db.Integer, primary_key=True, index=True)
     number = db.Column(db.Integer)
+    aglomachine_id = db.Column(db.ForeignKey('aglomachine.id'))
 
     # роторы и подшипники
     rotors = db.relationship('Rotor', backref='eksgauster', lazy='dynamic')
     bearings = db.relationship('Bearing', backref='eksgauster', lazy='joined')
 
-    aglomachine_id = db.Column(db.ForeignKey('aglomachine.id'))
+    datas = db.relationship('EksgausterData', backref='eksgauster',
+                            lazy='dynamic')
+
 
 
 @dataclass
 class EksgausterData(db.Model):
     """Данные эксгаустера"""
     id: int = db.Column(db.Integer, primary_key=True, index=True)
+    eksgauster_id = db.Column(db.ForeignKey('eksgauster.id'))
     added_at = db.Column(db.DateTime, default=db.func.now())
 
     # Маслобак
@@ -51,12 +55,17 @@ class EksgausterData(db.Model):
     oil_temperature_after: int = db.Column(db.Integer)
 
 
+
+
 @dataclass
 class Rotor(db.Model):
     """Ротор"""
 
     id: int = db.Column(db.Integer, primary_key=True, index=True)
     eksgauster_id = db.Column(db.ForeignKey('eksgauster.id'))
+
+    datas = db.relationship('RotorData', backref='rotor', lazy='dynamic')
+
 
 
 @dataclass
@@ -66,6 +75,9 @@ class RotorData(db.Model):
     id: int = db.Column(db.Integer, primary_key=True, index=True)
     added_at = db.Column(db.DateTime, default=db.func.now())
 
+    rotor_id = db.Column(db.ForeignKey('rotor.id'))
+
+
 
 @dataclass
 class Bearing(db.Model):
@@ -74,11 +86,15 @@ class Bearing(db.Model):
     id: int = db.Column(db.Integer, primary_key=True, index=True)
     eksgauster_id = db.Column(db.ForeignKey('eksgauster.id'))
 
+    datas = db.relationship('BearingData', backref='bearing', lazy='dynamic')
+
+
 
 @dataclass
 class BearingData(db.Model):
     """Данные подшипника"""
     id: int = db.Column(db.Integer, primary_key=True, index=True)
+    bearing_id = db.Column(db.ForeignKey('bearing.id'))
     added_at = db.Column(db.DateTime, default=db.func.now())
 
     temperature: float = db.Column(db.Float)
