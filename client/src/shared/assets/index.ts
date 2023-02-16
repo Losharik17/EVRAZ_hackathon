670fc96aa166a0dcde14def8d1d39add19e2b915
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-02-17 02:32:03 +0300

    Added icons

--- client/src/shared/assets/index.ts
+++ client/src/shared/assets/index.ts
@@ -1,7 +1,9 @@
 import evraz from './icons/evraz.svg';
 import exhauster from './images/example.jpg';
 import arrow from './icons/arrow.svg';
+import mark from './icons/mark.svg';
 
 export const EvrazIcon = evraz;
 export const Exhauster = exhauster;
 export const Arrow = arrow;
+export const Mark = mark;
