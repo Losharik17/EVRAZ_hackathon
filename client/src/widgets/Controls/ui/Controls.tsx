66095873774de74317e1411bcdcda766b53d6ccd
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-02-17 02:33:05 +0300

    TODO: Configure redux with filters and connect it to UI

--- client/src/widgets/Controls/ui/Controls.tsx
+++ client/src/widgets/Controls/ui/Controls.tsx
@@ -1,12 +1,15 @@
 import {
-    TrendsContorlsProperty,
+    TrendsContolsProperty,
     TrendsControlsLabel,
     TrendsControlsWrapper,
 } from 'entities/trends/ui/TrendsControls';
+import { TrendsFilterDropdown } from 'features/TrendsFilterDropdown';
+import { TrendsFilterToggle } from 'features/TrendsFilterToggle/ui/TrendsFilterToggle';
+import { WarningIndicator } from 'features/WarningIndicator';
 import { FC } from 'react';
 import { Arrow } from 'shared/assets';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { Button } from 'shared/ui';
+import { Button, Checkbox } from 'shared/ui';
 import cls from './Controls.module.scss';
 
 interface ControlsProps {
@@ -29,49 +32,53 @@ export const Controls: FC<ControlsProps> = ({ className }) => (
             )}
             body={(
                 <>
-                    <TrendsContorlsProperty
-                        before={(
-                            <Button className={cls.CustomButton}>
-                                <Arrow />
-                            </Button>
-                        )}
-                        title={<div>Главный привод</div>}
-                    />
-                    <TrendsContorlsProperty
-                        before={(
-                            <Button className={cls.CustomButton}>
-                                <Arrow />
-                            </Button>
-                        )}
-                        title={<div>Главный привод</div>}
-                    />
-                    <TrendsContorlsProperty
-                        before={(
-                            <Button className={cls.CustomButton}>
-                                <Arrow />
-                            </Button>
-                        )}
-                        title={<div>Главный привод</div>}
-                    />
-                    <TrendsContorlsProperty
-                        before={(
-                            <Button className={cls.CustomButton}>
-                                <Arrow />
-                            </Button>
-                        )}
-                        title={<div>Главный привод</div>}
-                    />
-                    <TrendsContorlsProperty
-                        before={(
-                            <Button className={cls.CustomButton}>
-                                <Arrow />
-                            </Button>
-                        )}
-                        title={<div>Главный привод</div>}
-                    />
-                    <TrendsContorlsProperty
-                        title={<div>Охладитель</div>}
-                    />
+                    <TrendsFilterDropdown
+                        title={<>Подшипник</>}
+                    >
+                        <TrendsFilterDropdown
+                            title={<>Подшипник</>}
+                        >
+                            <TrendsContolsProperty
+                                title={(
+                                    <>
+                                        <TrendsFilterToggle />
+                                        <div>СП5</div>
+                                    </>
+                                )}
+                                value={(
+                                    <WarningIndicator>
+                                        0000
+                                    </WarningIndicator>
+                                )}
+                            />
+                            <TrendsContolsProperty
+                                title={(
+                                    <>
+                                        <TrendsFilterToggle />
+                                        <div>СП5</div>
+                                    </>
+                                )}
+                                value={(
+                                    <WarningIndicator>
+                                        0000
+                                    </WarningIndicator>
+                                )}
+                            />
+                            <TrendsContolsProperty
+                                title={(
+                                    <>
+                                        <TrendsFilterToggle />
+                                        <div>СП5</div>
+                                    </>
+                                )}
+                                value={(
+                                    <WarningIndicator>
+                                        0000
+                                    </WarningIndicator>
+                                )}
+                            />
+                        </TrendsFilterDropdown>
+                    </TrendsFilterDropdown>
                 </>
             )}
         />
