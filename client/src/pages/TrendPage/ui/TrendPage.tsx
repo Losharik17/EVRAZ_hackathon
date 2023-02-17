e16a19bb028adc418f2559cc552519be955f422c
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-02-17 19:17:10 +0300

    Completed controls, TODO: Add redux

--- client/src/pages/TrendPage/ui/TrendPage.tsx
+++ client/src/pages/TrendPage/ui/TrendPage.tsx
@@ -1,8 +1,5 @@
+import { useTrendsSelector } from 'entities/trends/model';
 import { FC } from 'react';
-import {
-    LineChart, CartesianGrid,
-    Line, XAxis, YAxis, Tooltip, Area, AreaChart, ResponsiveContainer,
-} from 'recharts';
 import { classNames } from 'shared/lib';
 import { Controls } from 'widgets/Controls';
 import cls from './TrendPage.module.scss';
@@ -11,10 +8,14 @@ interface TrendPageProps {
     className?: string;
 }
 
-const TrendPage: FC<TrendPageProps> = ({ className }) => (
-    <div className={classNames(cls.TrendPage, {}, [className])}>
-        <Controls />
-    </div>
-);
+const TrendPage: FC<TrendPageProps> = ({ className }) => {
+    const filters = useTrendsSelector((state) => state.trends.filters);
+    return (
+        <div className={classNames(cls.TrendPage, {}, [className])}>
+            <Controls />
+            {filters.map((filter) => (<div key={filter}>{filter}</div>))}
+        </div>
+    );
+};
 
 export default TrendPage;
