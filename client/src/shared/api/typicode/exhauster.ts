7b535a42c09db25c63518ac0b3e360758f89eb49
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-02-18 14:32:55 +0300

    Exampled backend types

--- client/src/shared/api/typicode/exhauster.ts
+++ client/src/shared/api/typicode/exhauster.ts
@@ -1,46 +1,86 @@
-export type Status = 'ok' | 'warning' | 'critical'
+export type Status = 'idle' | 'warning' | 'critical'
 
 export interface Property {
     value: {
         number: number;
-        status: Status;
+        status?: Status;
     },
     title: string;
 }
 
-export type BearingProperty = Property
-export type OilProperty = Property
-export type RotorProperty = Property
-export type StatorProperty = Property
+export interface BearingParameters {
+    parameters: Property[];
+    id: number;
+    added_at: string;
+}
 
 export interface Bearing {
     id: number;
-    bearing_id: number;
-    added_at: string;
-    properties: BearingProperty[];
+    number: number;
+    datas: BearingParameters[];
 }
 
 export interface Oil {
-    level: OilProperty;
-    pressure: OilProperty;
+    oil_level: Property;
+    oil_pressure: Property;
+}
+
+export interface OilPump {
+    starting_oil_pump_started: Property;
+    emergency_oil_pump_started: Property;
+}
+
+export interface Cooler {
+    water_temperature_before: Property;
+    water_temperature_after: Property;
+    oil_temperature_before: Property;
+    oil_temperature_after: Property;
+}
+
+export interface MainDrive {
+    rotor_current: Property;
+    rotor_voltage: Property;
+    stator_current: Property;
+    stator_voltage: Property;
+    stator_temperature: Property;
+}
+
+export interface Manifold {
+    collector_temperature_before: Property;
+    collector_underpressure_before: Property;
+}
+
+export interface Operations {
+    work: Property;
+    motor_air_temperature_1: Property;
+    motor_air_temperature_2: Property;
+    motor_air_temperature_3: Property;
+    temperature_front_eksgauster: Property;
+    vacuum_front_eksgauster: Property;
+}
+
+export interface SensorsData {
+    id: number;
+    oil: Oil;
+    oil_pump: OilPump;
+    cooler: Cooler,
+    gas_manifold: Manifold;
+    eksgauster_operation: Operations;
+    added_at: string;
+    main_drive: MainDrive;
+    gas_valve_position: Property;
 }
 
 export interface Rotor {
-    current: RotorProperty;
-    voltage: RotorProperty;
-}
-
-export interface Stator {
-    current: StatorProperty;
-    voltage: StatorProperty;
-    temperature: StatorProperty;
+    id: number;
+    number: string;
+    start_date: string;
 }
 
 export interface Exhauster {
     id: number;
     name: string;
-    aglomachine_id: number;
     bearings: Bearing[];
-    oil: Oil;
-    stator: Stator;
+    datas: SensorsData[];
+    rotor: Rotor;
 }
